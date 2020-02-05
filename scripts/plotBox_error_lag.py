1e70a995c1d93f366812ef2d7f28613a30321011
user <maxime.beauchamp76@gmail.com>
2020-02-05 13:39:26 +0100

    Fit variance error f2lag

--- scripts/plotBox_error_lag.py
+++ scripts/plotBox_error_lag.py
@@ -1,9 +1,10 @@
 from pb_anda import *
 import matplotlib.dates as mdates
 
+datapath = "/home/user/Bureau/NATL60/DATA"
 file = netCDF4.Dataset(datapath+"/"+"data/dataset_nadir_5d.nc",'r')
 ssh_obs = file.variables["ssh_obs"][:].data.astype('float64').flatten()
-lag     = file.variables["lag"][:].data.astwhiskerpropsype('float64').flatten()
+lag     = file.variables["lag"][:].data.astype('float64').flatten()
 
 file2 = netCDF4.Dataset(datapath+"/"+"maps/NATL60-CJM165_ssh_y2013.1y.nc",'r')
 ssh_mod = file2.variables["ssh"][:].data.astype('float64').flatten()
@@ -20,7 +21,13 @@ medianprops = dict(linestyle='-', linewidth=1, color='k')
 fig, ax = plt.subplots(figsize=(10,7))
 var= dfr.groupby("lag").error.agg(np.var)
 var.index = np.arange(1,len(var)+1)
-ax.plot(var,'r-')
+# fit variance=f(lag)
+coeff = np.polyfit(np.unique(lag), var, 2)
+fit = pd.Series(np.polyval(coeff, np.unique(lag)))
+fit.index = np.arange(1,len(var)+1)
+ax.plot(fit,'k-',linewidth=2,label=r"$\mathrm{\widehat{Var}}[y-Hx | lag]=f(lag)$")
+ax.plot(var,'r--',linewidth=1.25,label=r"$\mathrm{Var}[y-Hx | lag]$")
+# boxplot
 bp = dfr.boxplot(column="error",by="lag", showfliers=False,\
 boxprops=boxprops,medianprops=medianprops,ax=ax, rot=45) 
 ticks = ax.xaxis.get_ticklocs()
@@ -28,10 +35,13 @@ ticklabels = [l.get_text() for l in ax.xaxis.get_ticklabels()]
 ax.xaxis.set_ticks(ticks[::10])
 ax.xaxis.set_ticklabels(ticklabels[::10])
 ax.set_ylim([-0.25,0.25])
+ax.legend()
 plt.title('')
 fig.suptitle('')
 plt.savefig(datapath+"/"+"data/boxplot_error_lag.pdf")
 plt.close()
+
+
 
 
 
